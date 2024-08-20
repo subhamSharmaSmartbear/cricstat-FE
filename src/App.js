54aae7f5d73a910fbed64ce80179c933d3342d46
subham sharma <subham.sharma@smartbear.com>
2024-08-20 13:59:20 +0530

    push login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+
 
 function App() {
+  // const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="*" element={<DefaultContainer />} /> */}
+
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// function DefaultContainer({ loader }) {
+//   return (
+//     <div className="w-[100vw] h-[100%] flex items-center  overflow-x-hidden">
+//       <Sidebar />
+//       <div className="w-[93vw] h-[100vh]  flex flex-col main overflow-hidden">
+//         <Header />
+//         <div className="w-[100%] h-[90vh] overflow-auto">
+//           <Routes>
+//             <Route
+//               path="/dashboard"
+//               element={user ? <Dashboard /> : <Navigate to="/" />}
+//             />
+            
+//           </Routes>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
