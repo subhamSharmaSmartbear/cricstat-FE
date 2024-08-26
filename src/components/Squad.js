8c5e72c4717f1f29703438d0238f11fbbf064111
subham sharma <subham.sharma@smartbear.com>
2024-08-26 12:49:42 +0530

    push team

---
+++ src/components/Squad.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Squad = () => {
+  return (
+    <div className='w-[100%] border h-[55%]  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 overflow-y-scroll scrollable-content custom-scrollbar  '>
+      <div className='w-[20rem] h-[100%]  bg-[#434343] rounded-[10px]'></div>
+      <div className='w-[20rem] h-[100%]  bg-[#434343] rounded-[10px]'></div>
+      <div className='w-[20rem] h-[100%]  bg-[#434343] rounded-[10px]'></div>
+      <div className='w-[20rem] h-[100%]  bg-[#434343] rounded-[10px]'></div>
+      <div className='w-[20rem] h-[100%]  bg-[#434343] rounded-[10px]'></div>
+    </div>
+  )
+}
+
+export default Squad
